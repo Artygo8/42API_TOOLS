f8e435a174ed9776a657eeff3b737a4b6b46bbe6
Artygo8 <arthur.gossuin@gmail.com>
2020-12-20 10:45:49 +0100

    little cleaner, still working

--- json_getter.py
+++ json_getter.py
@@ -10,32 +10,66 @@ def rm_f(path):
     if os.path.exists(path):
         os.remove(path)
 
+
 # retrieve_txt(json_file_name, what_we_want_as_txt, where_we_get_the_info)
 # Example usage:
-# retrieve("users_57131_scale_teams.json", where=["team", "project_gitlab_path"], what=["questions_with_answers","guidelines"], title = "name")
+#
+#   retrieve(
+#       json_file_name = "users_57131_scale_teams.json",
+#       where = ["team", "project_gitlab_path"],
+#       what = ["questions_with_answers","guidelines"],
+#       title = "name"
+#   )
+#
 
-def retrieve_txt(json_file, where, what, title=None): # what must be len=2 for now
+def retrieve_txt(json_file_name, where, what, title=None): # what must be len=2 for now
 
-    BASEDIR="retrievals/"
-    with open(json_file) as my_json:
-        scale_teams = json.load(my_json)
+    BASEDIR="evals/"
+    with open(json_file_name) as json_file:
+        json_datas = json.load(json_file)
 
-        for st in scale_teams:
-            file_name = st
-            for i in range(len(where)):
-                file_name = file_name[where[i]]
-            folder, file_name = file_name.split('/')[-2:]
-            file_name += "txt"
+    for data in json_datas:
 
-            mkdir_p(BASEDIR + folder)
+        # Name of the file we will put things in. ex: "pedago_world/42-cursus/inner-circle/libft"
+        file_name = data
+        for i in range(len(where)):
+            file_name = file_name[where[i]]
 
-            with open(BASEDIR + folder + '/' + file_name,"w+") as f:
-                for i, qwa in enumerate(st[what[0]]):
-                    if title:
-                        f.write(str(i) + ") " + qwa[title] + "\n\n")
-                    f.write(qwa[what[1]] + "\n\n")
+        # we take only the last part of this name. ex: folder = inner-circle, file = libft.txt
+        folder, file_name = file_name.split('/')[-2:]
+        file_name += "txt"
+        mkdir_p(BASEDIR + folder)
+
+        with open(BASEDIR + folder + '/' + file_name,"w+") as f:
+            for i, main_content in enumerate(data[what[0]]):
+                if title:
+                    f.write(str(i) + ") " + main_content[title] + "\n\n")
+                f.write(main_content[what[1]] + "\n\n")
     
-# retrieve("users_57131_scale_teams.json", where=["team", "project_gitlab_path"], what=["questions_with_answers","guidelines"])
+
+def retrieve_json(session, path):
+
+    mkdir_p('/'.join(path.split('/')[:-1]))
+    rm_f(path + ".json")
+
+    with open(path + ".json", "a") as json_file:
+
+        content = []
+        for number in range( 0, 100 ) :
+            time.sleep(1)
+            web_page = session.get(f'https://api.intra.42.fr/v2/{path}?page[number]={number}', params={'format': 'json'})
+            print("Status code:", web_page.status_code)
+            tmp = eval(str(web_page.json()))
+            if tmp == []:
+                break
+            content.extend(tmp)
+
+        json.dump(content, json_file, indent=4)
+
+
+def retrieve_data(json_file_name, what, content):
+    pass
+
 
 class OAuth42:
 
@@ -64,27 +98,14 @@ class OAuth42:
         self.session = OAuth2Session(self.client_id, self.client_secret, access_token = self.token)
         return self.session
 
+
+
+
+
 # Before trying to retrieve anything :
 connection = OAuth42()
 connection.access_token()
 session = connection.get_session()
 
-def retrieve_json(path):
-    mkdir_p('/'.join(path.split('/')[:-1]))
-    rm_f(path + ".json")
-    with open(path + ".json", "a") as json_file:
-        # json_file.write('[')
-        content = []
-        for number in range( 0, 100 ) :
-            time.sleep(1)
-            web_page = session.get(f'https://api.intra.42.fr/v2/{path}?page[number]={number}', params={'format': 'json'})
-            print("Status code:", web_page.status_code)
-            tmp = eval(str(web_page.json()))
-            if tmp == []:
-                break
-            content.extend(tmp)
-
-        json.dump(content, json_file, indent=4)
-
-retrieve_json("users/57131/scale_teams")
+retrieve_json(session, "users/57131/scale_teams")
 retrieve_txt("users/57131/scale_teams.json", where=["team", "project_gitlab_path"], what=["questions_with_answers","guidelines"], title = "name")
